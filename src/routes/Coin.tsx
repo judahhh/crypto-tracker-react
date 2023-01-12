08801a813a95aab1cb17288e564ac29477d28853
judahhh <jdh99080516@gmail.com>
2023-01-12 16:42:48 +0900

    feat: 컴포넌트 간 코인 정보 보내기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,9 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
+import Chart from "./Chart";
+import Price from "./Price";
 import styled from "styled-components";
+
 const Container = styled.div`
   padding: 0px 20px;
   max-width: 480px;
@@ -22,6 +25,30 @@ const Loader = styled.span`
   text-align: center;
   display: block;
 `;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
 interface RouteParams {
   state: {
     // coinID: string;
@@ -29,12 +56,67 @@ interface RouteParams {
     rank: number;
   };
 }
+
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
 function Coin() {
   const [loading, setLoading] = useState(false);
   const { coinID } = useParams();
   const { state } = useLocation() as RouteParams;
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
 
   useEffect(() => {
     (async () => {
@@ -43,24 +125,28 @@ function Coin() {
       );
       const json = await infoData.json();
 
+      console.log(json);
       const priceData = await fetch(
         `https://api.coinpaprika.com/v1/tickers/${coinID}`
       );
       const json2 = await priceData.json();
-      setInfo(infoData);
-      setPriceInfo(priceData);
-      console.log(infoData, priceData);
+      console.log(json2);
+      setInfo(info);
+      setPriceInfo(priceInfo);
+      setLoading(false);
       //위 두줄을 한줄로 줄이기
       /* const reponse=await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinID}`
-      ).json(); )*/
+      ).json() )*/
     })();
-  }, []);
+  }, [coinID]);
 
   return (
     <Container>
       <Header>
-        <Title>코인{state?.name || "Loading"}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
       </Header>
       {loading ? <Loader>Loading...</Loader> : null}
     </Container>
